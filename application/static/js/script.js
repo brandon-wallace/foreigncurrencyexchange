bdf0cbf27a477bfcc0a1c79c9fee18ee6e5c2e70
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-03-14 18:31:13 -0400

    Set datetime

--- application/static/js/script.js
+++ application/static/js/script.js
@@ -4,17 +4,6 @@
 const date = new Date();
 
 (function() {
-    date.toLocaleString('en-US', { 
-        hour12: false, 
-        year: 'numeric', 
-        month: 'long', 
-        day: 'numeric', 
-        weekday: 'long', 
-        timeZone: 'UTC', 
-        minute: '2-digit', 
-        second: '2-digit', 
-        second: '2-digit', 
-        hour: '2-digit' 
-    });
-    document.querySelector('.date-and-time').textContent = date;
+    date.toLocaleString({ 
+    document.querySelector('.date_time').textContent = date;
 })();
