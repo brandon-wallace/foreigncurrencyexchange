cf60968be55936c92ff1a0a50235a05a37c87ca6
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-03-26 16:35:02 -0400

    fix: Display historical table data
    The table will contain default values of 0.00000 as a placeholder
    until a query is made to the API.

--- application/static/js/script.js
+++ application/static/js/script.js
@@ -1,13 +1,1 @@
 // application/static/js/script.js
-
-
-const form = document.querySelector('form');
-const table = document.querySelector('table');
-
-
-const displayTable = () => {
-    document.getElementById('hide').style.display = 'block';
-}
-
-
-form.addEventListener('submit', setTimeout(displayTable, 500), false);
